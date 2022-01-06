fd52da6c00d69729e52c6f0ea1acdf3abc7b7f84
Suryo Mujahid <suryomujahid@gmail.com>
2022-01-06 11:59:50 +0700

    little changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
-const selectRayon = $('#rayon');
-let listRayon = [];
-
 totalRayon.forEach(function(val) {
+    let listRayon = [];
     for (let i = 1; i <= parseInt(val.split("_").pop()); i++) {
-        let rayon = val.substring(0, val.length - 1) + i;
+        let namaRayon = val.substring(0, val.length - 1) + i;
 
-        listRayon.push(rayon);
-        selectRayon.html(selectRayon.html() + `<option value="${rayon}">${rayon}</option>`);
+        listRayon.push(namaRayon);
+        rayon.html(rayon.html() + `<option value="${namaRayon}">${namaRayon}</option>`);
     }
 });
 
@@ -17,8 +15,7 @@ $(function(){
 
 $('#gas').click(function() {
     if (timeStart.val() === '' || noAbsen.val() === '') {
-        console.log(isIslam.is(':checked'), noAbsen.val(), timeStart.val())
-        return alert('isi lkh rayon / no absen / agama');
+        return alert('isi dimulai tgl / no absen');
     }
 
     let formId = idsRayon[rayon.prop('selectedIndex')];
@@ -32,13 +29,16 @@ const spamLKH = async (formId) => {
     
     let time = new Date(timeStart.val());
     let today = new Date();
+    const btnProcess = $('#gas');
 
     for (time; time <= today; time.setDate(time.getDate() + 1)) {
-        $('#loading').html('cek inspect element -> console <bold>mas</bold>')
+        $('#gas').prop('disabled', true);
+        $('#loading').html('cek inspect element -> console <bold>mas</bold>.')
         if (time.getDay() === 0) continue;
 
         await submitForm(formId, time, fillQuestion);
     }
 
+    $('#gas').prop('disabled', false);
     $('#loading').html('');
 };
